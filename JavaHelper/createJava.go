832ec21c72283d6d6d8bf379de84e2468c0f41ad
Connor Knabe <connorknabe@gmail.com>
2014-09-29 16:00:09 -0500

    Prints out everyting correcntly

--- JavaHelper/createJava.go
+++ JavaHelper/createJava.go
@@ -10,11 +10,12 @@ func main() {
 	if (len(os.Args)<2){
 		fmt.Printf("You need one argument!\n");
 	} else {
-		fName := os.Args[1]+".java"
+		fName := ""
+		fName = os.Args[1]+".java"
 		_, err := os.Open(fName)
 		if err != nil {
 		    // no such file or dir
-			fmt.Printf("Creating File!")
+			fmt.Printf("Creating File!",fName)
 			newJavaFile, err := os.Create(fName)
 			n, err := io.WriteString(newJavaFile, "public class " + os.Args[1] + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
 			    if err != nil {
@@ -23,19 +24,20 @@ func main() {
 		    return
 		} else {
 			fmt.Printf("Compiling and running file\n")
-
 			cmd := exec.Command("javac", fName)
+			fName = os.Args[1]
 
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			cmd.Stdin = os.Stdin
 			cmd.Start()
-			fmt.Printf(os.Args[1])
+			//cmd.Wait()
+			fmt.Printf("Java console output below:\n");
 			cmd = exec.Command("java", os.Args[1])
 			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
 			cmd.Start()
+			cmd.Wait()
+			fmt.Printf("Done")
 
 
 		}

57e4bc3383e8c207e706edb387c08745fbb4400e
Connor Knabe <connorknabe@gmail.com>
2014-09-29 10:38:57 -0500

    Executing new pipe

--- JavaHelper/createJava.go
+++ JavaHelper/createJava.go
@@ -1,19 +1,19 @@
 package main
 
 import "os"
-//import "os/exec"
+import "os/exec"
 import "fmt"
 import "io"
 
 func main() {
 
 	if (len(os.Args)<2){
-		fmt.Printf("You need one argument!");
+		fmt.Printf("You need one argument!\n");
 	} else {
 		_, err := os.Open(os.Args[1]+".java")
 		if err != nil {
 		    // no such file or dir
-			fmt.Printf("File not found")
+			fmt.Printf("Creating File!")
 			newJavaFile, err := os.Create(os.Args[1]+".java")
 			n, err := io.WriteString(newJavaFile, "public class " + os.Args[1] + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
 			    if err != nil {
@@ -21,8 +21,13 @@ func main() {
 			    }
 		    return
 		} else {
+			fmt.Printf("Compiling and running file\n")
 
-			fmt.Printf("HI")
+			cmd := exec.Command("echo hi")
+			cmd.Start()
+
+
+
 		}
 
 

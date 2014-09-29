99eaf6fdbe070ef5460b8943b1f03090eac7147a
Connor Knabe <connorknabe@gmail.com>
2014-09-29 10:21:59 -0500

    Creates file

--- JavaHelper/createJava.go
+++ JavaHelper/createJava.go
@@ -3,14 +3,29 @@ package main
 import "os"
 //import "os/exec"
 import "fmt"
-
+import "io"
 
 func main() {
 
 	if (len(os.Args)<2){
 		fmt.Printf("You need one argument!");
 	} else {
-		fmt.Printf("Ok")
+		_, err := os.Open(os.Args[1]+".java")
+		if err != nil {
+		    // no such file or dir
+			fmt.Printf("File not found")
+			w, err := os.Create(os.Args[1])
+			n, err := io.WriteString(w, "blahblahblah")
+			    if err != nil {
+			        fmt.Println(n, err)
+			    }
+		    return
+		} else {
+
+			fmt.Printf("HI")
+		}
+
+
 	}
 
 

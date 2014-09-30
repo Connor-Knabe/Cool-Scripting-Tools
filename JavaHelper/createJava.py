3c9f35d639499dc02fee11394e65b9240b67c0d4
Connor Knabe <connorknabe@gmail.com>
2014-09-30 15:33:31 -0500

    Code looking good

--- JavaHelper/createJava.py
+++ JavaHelper/createJava.py
@@ -3,19 +3,13 @@ import os
 import subprocess
 
 #to run python createJava.py fileNamehere
-if(len(sys.argv)<2):
-	print ("Usage: createJava.py fileName")
-	sys.exit(0)
-fName = sys.argv[1]
-
-def compile(fName):
-    print ("Compiling")
-    subprocess.call("javac " + fName, shell=True)
-    print ("Output below\n")
-    subprocess.call("java " + fName.split('.java')[0] +" ", shell=True)
-
-
 def main():
+	if(len(sys.argv)<2):
+		print ("Usage: createJava.py fileName")
+		sys.exit(0)
+		
+	fName = sys.argv[1]
+
 	if(os.path.isfile(fName+".java")):
 		compile(fName+".java")
 	elif (os.path.isfile(fName)):
@@ -24,7 +18,12 @@ def main():
 	    print ("New file created")
 	    file = open(fName + ".java", 'w+')
 	    file.write("public class " + fName + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
-    
+
+def compile(fName):
+    print ("Compiling")
+    subprocess.call("javac " + fName, shell=True)
+    print ("Output below\n")
+    subprocess.call("java " + fName.split('.java')[0] +" ", shell=True)
 
 if __name__ == '__main__':
 	main()

5cc64e2e6786a88e66df6df2441b153668fdc88a
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 11:49:04 -0500

    Now checks for boolean properly

--- MkdirCd/mkdirCd.sh
+++ MkdirCd/mkdirCd.sh
@@ -6,17 +6,31 @@ openFlag=false;
 while getopts :o: option
 do
 	case "$option" in
-    o) openFlag = true
+    o) 
+        openFlag = true
+        ;;
+    *)
+        echo "Something went wrong"
+        ;;
+    esac
 		
-
+done
 
 if [ -z "$1" ];then
 	echo "Please enter folder name"
 	exit
 fi
-mkdir $1
-cd $1
 
-if $openFlag = true ; then
+
+if [ "$openFlag" ] ; then 
+    echo 'Is true'
+    mkdir $1
+    cd $1
+else 
+    echo 'Is false'
+fi
+
+
+if $openFlag ; then
 	jcc $1
 fi 

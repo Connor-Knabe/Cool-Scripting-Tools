b1aca8f6f3c1cc0609bfc8c9cf9bc19f883c6c4e
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 15:23:02 -0500

    Seems to be working properly

--- MkdirCd/mkdirCd.sh
+++ MkdirCd/mkdirCd.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 #Usage: make an alias = ". /mkdirCd.sh"
 
-openFlag=false;
-echo "Test"
-echo $openFlag
-
-while getopts ":o:" option; do
-
-	case "$option" in
-    o) 
-        echo "TRUE"
-        openFlag = true
-        ;;
-    *)
-        echo "Something went wrong"
-        ;;
-    esac
-		
-done
-
 if [ -z "$1" ];then
 	echo "Please enter folder name"
 	exit
 fi
 
-echo "Dir Name: " + $2
-
-echo $openFlag
-
-if [ "$openFlag" = true ] ; then 
-    echo 'Is true'
-    echo $openFlag
+if [ $1 = "-o" ];then
     mkdir $2
     cd $2
     jcc $2
-
-else 
-    echo 'Is false'
+else
     mkdir $1
     cd $1
 fi

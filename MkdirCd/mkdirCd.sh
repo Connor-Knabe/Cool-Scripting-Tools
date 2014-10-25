bc9a26c63d354018d98d13c4afc05652245b3a25
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 11:33:23 -0500

    Working adding flags to open the file created in the new directory

--- MkdirCd/mkdirCd.sh
+++ MkdirCd/mkdirCd.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 #Usage: make an alias = ". /mkdirCd.sh"
+
+openFlag = false
+
+while getopts :o: option
+do
+	case "$option" in
+    o) openFlag = true
+		
+
+
 if [ -z "$1" ];then
 	echo "Please enter folder name"
 	exit
 fi
 mkdir $1
 cd $1
+
+if ["$openFlag" = true]; then
+	jcc $1
+fi 
